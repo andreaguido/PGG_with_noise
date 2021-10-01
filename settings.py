31929acf4f68764177f7e50e4d25760dcd31b043
Andrea Guido <andrea.guido.research@gmail.com>
2021-10-01 16:36:22 +0200

    setup files

--- settings.py
+++ settings.py
@@ -3,19 +3,10 @@ from os import environ
 
 SESSION_CONFIGS = [
     dict(
-        name='guess_two_thirds',
-        display_name="Guess 2/3 of the Average",
-        app_sequence=['guess_two_thirds', 'payment_info'],
-        num_demo_participants=3,
-    ),
-    dict(
         name='public_goods_with_noise',
         display_name="PGGwNoise",
-        app_sequence=['public_goods_with_noise', 'payment_info'],
+        app_sequence=['public_goods_with_noise'],
         num_demo_participants=4,
-    ),
-    dict(
-        name='survey', app_sequence=['survey', 'payment_info'], num_demo_participants=1
     ),
 ]
 
