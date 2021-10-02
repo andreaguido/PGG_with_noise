cd16908bfb6a9adaaa11b16db928e05b66fbb744
Andrea Martinangeli <andy.martinangeli@gmail.com>
2021-10-02 10:36:43 +0200

    Changed real world currency
    1 Point = 0.005 real world currency (half a euro cent), so that the final payoff is directly readable in Euros

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ SESSION_CONFIGS = [
 # e.g. self.session.config['participation_fee']
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=0.50, participation_fee=0.00, doc=""
+    real_world_currency_per_point=0.005, participation_fee=0.00, doc=""
 )
 
 PARTICIPANT_FIELDS = ['final_payment_euros']
